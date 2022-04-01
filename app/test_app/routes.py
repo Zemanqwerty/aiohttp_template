b3bbff16f11d3b7de6f88ae5e019e2ddcdebfe9d
zeman <82312679+Zemanqwerty@users.noreply.github.com>
2022-04-01 20:47:44 +0800

    Update routes.py

--- app/test_app/routes.py
+++ app/test_app/routes.py
@@ -7,6 +7,6 @@ def setup_routes(app):
     app.router.add_static('/css/', path='static/css', name='css')
     app.router.add_static('/img/', path='static/img', name='img')
     app.router.add_post('/api/authorization', views.api_authorization)
-    app.router.add_post('/api/regisration', views.api_registration)
+    app.router.add_post('/api/registration', views.api_registration)
     app.router.add_post('/api/logout', views.api_logout)
     app.router.add_get('/', views.test_page)
